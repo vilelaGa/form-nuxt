eec0c106a75e8bdda901009c6ffa73115416b661
Gabriel de Souza Lima Vilela <05845@base.sobeu.br>
2023-02-13 17:39:38 -0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><Logo /></a>
+        </div>
+      </nav>
+    </div>
+
+    <Nuxt />
+
+    <footer style="background: #00dc82" class="fixed-bottom p-4">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="col-md-4 text-center">
+            <p>2023</p>
+          </div>
+          <div class="col-md-4"></div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
